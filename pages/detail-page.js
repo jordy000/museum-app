979c1003bfb1823584a302842cec0bf1f2d1584c
Jordy <jfreriks@deloitte.nl>
2019-05-28 17:45:01 +0200

    javascript shit toegevoegd

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,19 @@
+function doesNotPassAllValidations(name, msg) {
+    if (msg.length < 1 || name.length < 1) {
+      alert('You forgot to fill in your name or message!')
+      return true;
+    }
+  
+    if(msg.length > 280) {
+      alert('Your comment is too long')
+      return true
+    }
+  
+    return false
+  }
+
+
+
 // add event handler
 function submitComment() {
     // gather data
@@ -11,8 +27,17 @@ function submitComment() {
     const h3 = document.createElement('h1')
     const p = document.createElement('p')
   
+    if(doesNotPassAllValidations(name, msg)){
+        return null
+      } 
+
+    const nameSubset = name.substr(0,1)
+    const capitalizedName = nameSubset.toUpperCase()+name.substr(1)
+
+    //console.log(capitalizedName)
+    
     // adjust the elements we created
-    h3.innerHTML = `${name} said:`
+    h3.innerHTML = capitalizedName+ ' said:'
     p.innerHTML = msg
     comment.classList.add('comment')
     comment.appendChild(h3)
@@ -21,8 +46,15 @@ function submitComment() {
     // display the elements on the page
     const commentSection = document.getElementById('comments')
     commentSection.appendChild(comment)
-  
+
+   
+    
+    
     // reset form values
     inputField.value = null
     textArea.value = null
+
+    
   }
+
+  
